5e61c2d0576a69369521b3595ab6814c2d99fc36
Mohammad Hashemi <89422926+Amahseyn@users.noreply.github.com>
2023-12-21 16:23:14 +0330

    Update 1inch28_LCD_test.py

--- 1inch28_LCD_test.py
+++ 1inch28_LCD_test.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python
-
-# -*- coding: UTF-8 -*-
-
 import os
 
 import sys
@@ -34,13 +30,31 @@ BL = 18
 
 bus = 0
 
-device_lcd1 = 0
+device_lcd1 = 1
 
-device_lcd2 = 1
+device_lcd2 = 0
 
 
 
 logging.basicConfig(level=logging.DEBUG)
+
+def load_images(folder_path):
+
+        images = []
+
+        for filename in sorted(os.listdir(folder_path)):
+
+            if filename.endswith(('.png', '.jpg', '.jpeg')):
+
+                image_path = os.path.join(folder_path, filename)
+
+                image = Image.open(image_path)
+
+                image = image.resize((240, 240))
+
+                images.append(image)
+
+        return images
 
 
 
@@ -52,23 +66,37 @@ def draw_count_text(draw, font, count):
 
 
 
-def count_to_10_lcd1(disp_lcd1):
+def count_to_100_lcd1():
 
-    image1 = Image.new("RGB", (disp_lcd1.width, disp_lcd1.height), "BLACK")
+    disp_lcd1 = LCD_1inch28.LCD_1inch28(spi=SPI.SpiDev(0, 0), rst=27, dc=25, bl=18)
 
-    draw1 = ImageDraw.Draw(image1)
+    disp_lcd1.Init()
 
-    font = ImageFont.load_default()
+    disp_lcd1.clear()
 
+    images = []
 
+    folder_path ='/home/a/bcm2835-1.71/WiringPi/LCD_Module_RPI_code/RaspberryPi/python/frames'
 
-    for count1 in range(100,200):
+    for filename in sorted(os.listdir(folder_path)):
 
-        draw_count_text(draw1, font, count1)
+            if filename.endswith(('.png', '.jpg', '.jpeg')):
 
-        disp_lcd1.ShowImage(image1.rotate(180))
+                image_path = os.path.join(folder_path, filename)
 
-        time.sleep(.1)
+                image = Image.open(image_path)
+
+                image = image.resize((240, 240))
+
+                images.append(image)
+
+                #images = sorted(images)
+
+    for image in images:
+
+        disp_lcd1.ShowImage(image.rotate(180))
+
+        time.sleep(0.1)
 
 
 
@@ -76,61 +104,53 @@ def count_to_10_lcd1(disp_lcd1):
 
 
 
-def count_to_minus_10_lcd2(disp_lcd2):
+def count_to_minus_100_lcd2():
 
-    image2 = Image.new("RGB", (disp_lcd2.width, disp_lcd2.height), "BLACK")
+    disp_lcd2 = LCD_1inch28.LCD_1inch28(spi=SPI.SpiDev(0, 1), rst=27, dc=25, bl=18)
 
-    draw2 = ImageDraw.Draw(image2)
+    disp_lcd2.Init()
 
-    font = ImageFont.load_default()
+    disp_lcd2.clear()
 
+    images = []
 
+    folder_path ='/home/a/bcm2835-1.71/WiringPi/LCD_Module_RPI_code/RaspberryPi/python/frames'
 
-    for count2 in range(0, -100, -1):
+    for filename in sorted(os.listdir(folder_path)):
 
-        draw_count_text(draw2, font, count2)
+            if filename.endswith(('.png', '.jpg', '.jpeg')):
 
-        disp_lcd2.ShowImage(image2.rotate(180))
+                image_path = os.path.join(folder_path, filename)
 
-        time.sleep(.1)
+                image = Image.open(image_path)
 
+                image = image.resize((240, 240))
 
+                images.append(image)
+
+                #images = sorted(images)
+
+    for image in images:
+
+        disp_lcd2.ShowImage(image.rotate(180))
+
+        time.sleep(0.1) 
 
     disp_lcd2.module_exit()
 
 
 
-try:
-
-    # Display with hardware SPI:
-
-    disp_lcd1 = LCD_1inch28.LCD_1inch28(spi=SPI.SpiDev(bus, device_lcd1), rst=RST, dc=DC, bl=BL)
-
-    disp_lcd2 = LCD_1inch28.LCD_1inch28(spi=SPI.SpiDev(bus, device_lcd2), rst=RST, dc=DC, bl=BL)
-
-
-
-    # Initialize libraries.
-
-    disp_lcd1.Init()
-
-    disp_lcd2.Init()
-
-
-
-    # Clear displays.
-
-    disp_lcd1.clear()
-
-    disp_lcd2.clear()
+try: 
 
 
 
     # Create and start threads for counting on each LCD
 
-    thread_lcd1 = threading.Thread(target=count_to_10_lcd1, args=(disp_lcd1,))
+    thread_lcd1 = threading.Thread(target=count_to_100_lcd1)
 
-    thread_lcd2 = threading.Thread(target=count_to_minus_10_lcd2, args=(disp_lcd2,))
+
+
+    thread_lcd2 = threading.Thread(target=count_to_minus_100_lcd2)
 
 
 
@@ -148,6 +168,8 @@ try:
 
 
 
+
+
     logging.info("quit:")
 
 
@@ -155,6 +177,8 @@ try:
 except IOError as e:
 
     logging.info(e)
+
+
 
 except KeyboardInterrupt:
 
